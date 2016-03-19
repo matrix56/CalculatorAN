005a73265cfe543a38aacabfb90bded24364c9fb
meandors <meandors@tlen.pl>
2016-03-19 12:41:51 +0100

    Dodana metoda silnia i dzielenie mod, oraz edytowałem interface w tym celu ;D Jest Błąd w klasie CalculatorService, ale nie wiem dokładnie czego chce.

--- app/src/main/java/pl/angrymarschmallow/calculatoran/CalculatorService.java
+++ app/src/main/java/pl/angrymarschmallow/calculatoran/CalculatorService.java
@@ -8,4 +8,21 @@ public class CalculatorService implements CalculatorEngine {
     // Miejsce na implementację metod z interfejsu :]
 
     // ! Piszcie komentarze do metod.
+
+
+    //dzielenie modularne ( a - licznik, b - dzielnik)
+    @Override
+    public int mod(int a, int b) {
+        return  a % b;
+    }
+
+    //silnia rekurencyjnie( a - stopien silni)
+    @Override
+    public int fact(int a) {
+        if (a < 1){
+            return 1;
+        }else {
+            return a * fact(a -1);
+        }
+    }
 }
